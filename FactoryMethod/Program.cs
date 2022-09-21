4ab9bba2e3a3f144b75ea3d9238d475527207e85
Yiğitcan Ölmez <yigitcan.olmez@affinitybox.com>
2022-09-21 16:38:30 +0300

    factorymethod

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new LoggerFactory());
+            customerManager.Save();
+            Console.ReadLine();
+        }
+    }
+    public class LoggerFactory : ILogger
+    {
+        public void Logger()
+        {
+
+        }
+    }
+    public interface ILoggerFactory
+    {
+    } 
+    public interface ILogger
+    {
+        void Logger();
+    }
+    public class YoLogger : ILogger
+    {
+        public void Logger()
+        {
+            Console.Write("Logged with YOLOGGER");
+        }
+    }
+    public class CustomerManager
+    {
+        private readonly ILogger _logger;
+        public CustomerManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Save()
+        {
+            _logger.Logger();
+            Console.WriteLine("SAVED");
+            
+        }
+    }
+}
