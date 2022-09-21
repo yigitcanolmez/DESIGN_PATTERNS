59a23506f802a0b25530f608db863a423fb7b8e4
Yiğitcan Ölmez <yigitcan.olmez@affinitybox.com>
2022-09-21 15:49:42 +0300

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager = CustomerManager.CreateAsSingleton();
+            customerManager.Add();
+        }
+    }
+    class CustomerManager
+    {
+        private static CustomerManager _customer;
+        static object _lockObject = new object();
+        private CustomerManager()
+        {
+
+        }
+
+        public static CustomerManager CreateAsSingleton()
+        {
+            lock(_lockObject)
+            {
+                if (_customer == null)
+                    _customer = new CustomerManager();
+            }
+            return _customer;
+        }
+        public void Add()
+        {
+            Console.WriteLine("Ürün eklendi!");
+        }
+    }
+}
